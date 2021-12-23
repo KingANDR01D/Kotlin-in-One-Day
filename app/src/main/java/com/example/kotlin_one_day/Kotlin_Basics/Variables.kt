cb99031adca9a633d71806c645a0c55fd3a5a4fb
MohamedGoush <mohamedgoush@gmail.com>
2021-12-24 01:54:25 +0530

    OkHttp Project

---
+++ app/src/main/java/com/example/kotlin_one_day/Kotlin_Basics/Variables.kt
@@ -0,0 +1,31 @@
+package com.example.kotlin_one_day.Kotlin_Basics
+
+
+fun main(){
+
+    /**
+     * This is the Basic Main Function in Kotlin Programming
+     */
+
+    /**
+     *
+     * Variable Creation in Kotlin
+     *
+     * var and val are Used to Create a Basic Kotlin Variables.
+     *
+     * var is a variable is assigned another Value.
+     *
+     * val is a variable it is not change its Value.
+     * val is a Constant.
+     *
+     */
+    var number =10;
+    number = 20;
+
+    val contantvariable = 10;
+
+//    contantvariable =20;  // It Gives an Error Because The val is not reassigned.
+    println(contantvariable)
+    println(number)
+
+}
--- app/src/main/java/com/example/kotlin_one_day/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_one_day/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.kotlin_one_day
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        try {
+            val client = OkHttpClient()
+            var url: String = "http://reqres.in/api/users?page=2"
+
+            val request = Request.Builder()
+                .url(url)
+                .build()
+
+            val response = client.newCall(request).execute()
+            var result = response.body?.string()
+            print(result)
+        } catch (err: Error) {
+            print("Error When Executing get Requiest:" + err.localizedMessage)
+        }
+
+
     }
 }
